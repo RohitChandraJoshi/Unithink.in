1b07aa2eec81a068d1d1e6158864c1b847a13d19
Your Name <your.email@example.com>
2024-09-13 00:42:44 +0530

    changes in server

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());  // Allow all origins
+app.options('*', cors()); 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })

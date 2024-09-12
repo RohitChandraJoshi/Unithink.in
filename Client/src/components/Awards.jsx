5bf25b08885926900bfb53d24569e9503ff09a3c
Your Name <your.email@example.com>
2024-09-13 00:33:06 +0530

    minor updates

--- Client/src/components/Awards.jsx
+++ Client/src/components/Awards.jsx
@@ -6,7 +6,7 @@ const awards = [
     title: 'Global Women in Business & Leadership Awards',
     year: 2023,
     description: 'by GCPIT',
-    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Faima.png?alt=media&token=eee1eef8-d7d4-4710-b37a-e7c398bad310',
+    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Fgcpit.png?alt=media&token=4599b54d-024b-47a2-9d1f-59070e93f2ff',
   },
   {
     title: 'Outstanding Management and Entrepreneurship Education Institute',
@@ -18,31 +18,31 @@ const awards = [
     title: 'Eminent Academician Award',
     year: 2022,
     description: 'by M S Ramaiah Foundation at RIMS Bangalore on 10th Sept',
-    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Fgcpit.png?alt=media&token=4599b54d-024b-47a2-9d1f-59070e93f2ff',
+    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2FMSRF%20logo.png?alt=media&token=f487a8c4-7845-435c-950e-f1e3fede137a',
   },
   {
     title: 'Best Academician Award',
     year: 2022,
     description: 'by St. Mother Theresa University',
-    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2FMSRF%20logo.png?alt=media&token=f487a8c4-7845-435c-950e-f1e3fede137a',
+    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Fstmothertheresauniversity_logo.jpeg?alt=media&token=4d92c352-790a-4fcf-a6b3-e398feead283',
   },
   {
     title: 'Certificate of Appreciation for OMC-Google Challenge',
     year: 2021,
     description: 'by INADES from 20th Oct 2020 to 15th March 2021',
-    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2FMTC%20Global%20awards.jpg?alt=media&token=ef822ed7-9df1-46e5-8e81-8aa68c8e8fd7',
+    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Fgoogle%20omc.png?alt=media&token=849443c5-b8df-4ad6-8aea-b31024acafdb',
   },
   {
     title: 'Outstanding Management Teacher Award',
     year: 2018,
     description: 'by MTC Global on 13th Sept',
-    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Fgoogle%20omc.png?alt=media&token=849443c5-b8df-4ad6-8aea-b31024acafdb',
+    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2FMTC%20Global%20awards.jpg?alt=media&token=ef822ed7-9df1-46e5-8e81-8aa68c8e8fd7',
   },
   {
     title: 'AIMA-Accredited Management Teacher',
     year: 2011,
     description: 'certified by the All India Management Association (AIMA)',
-    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Fstmothertheresauniversity_logo.jpeg?alt=media&token=4d92c352-790a-4fcf-a6b3-e398feead283',
+    imgSrc: 'https://firebasestorage.googleapis.com/v0/b/unithink-89630.appspot.com/o/awards%20icons%2Faima.png?alt=media&token=eee1eef8-d7d4-4710-b37a-e7c398bad310',
   },
 ];
 
